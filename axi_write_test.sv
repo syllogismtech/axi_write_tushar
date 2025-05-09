fc5a9188d1fccf5bc88c8bd96be4f4345b8cb652
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-09 12:45:59 +0530

    Update axi_write_test.sv

--- axi_write_test.sv
+++ axi_write_test.sv
@@ -1,19 +1,24 @@
 class axi_write_test extends uvm_test;
   `uvm_component_utils(axi_write_test)
 
+  axi_env env; 
+
   function new(string name, uvm_component parent);
     super.new(name, parent);
   endfunction
 
-  virtual task run_phase(uvm_phase phase);
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    env = axi_env::type_id::create("env", this);
+  endfunction
+
+  task run_phase(uvm_phase phase);
     axi_write_sequence seq;
-    uvm_sequencer #(axi_write_addr_txn) sequencer;
-    axi_master_write_driver driver;
+    phase.raise_objection(this);
 
     seq = axi_write_sequence::type_id::create("seq");
-    sequencer = uvm_sequencer #(axi_write_addr_txn)::type_id::create("sequencer", this);
-    driver = axi_master_write_driver::type_id::create("driver", this);
-    driver.seq_item_port.connect(sequencer.seq_item_export);
-    seq.start(sequencer);
+    seq.start(env.axi_agent.sequencer); 
+
+    phase.drop_objection(this);
   endtask
 endclass
