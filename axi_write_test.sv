8c150a958cc7d6ecdee8bf21b6911c2e20c880bb
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-09 12:47:34 +0530

    Update axi_write_test.sv

--- axi_write_test.sv
+++ axi_write_test.sv
@@ -1,7 +1,7 @@
 class axi_write_test extends uvm_test;
   `uvm_component_utils(axi_write_test)
 
-  axi_env env; 
+  axi_env env;  // Assuming you have an environment called axi_env
 
   function new(string name, uvm_component parent);
     super.new(name, parent);
@@ -17,7 +17,7 @@ class axi_write_test extends uvm_test;
     phase.raise_objection(this);
 
     seq = axi_write_sequence::type_id::create("seq");
-    seq.start(env.axi_agent.sequencer); 
+    seq.start(env.axi_agent.sequencer);  // Assuming your env has axi_agent with a sequencer
 
     phase.drop_objection(this);
   endtask
